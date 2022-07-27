0bbd44b1b45d6f24f2bdf2594ec46df0ff2b7e6b
Afroz Loya <mloya@accelaero.com>
2022-07-27 15:29:04 +0400

    backend changes for tag

--- api/src/main/java/superai/dayofglory/models/PicAnnotation.java
+++ api/src/main/java/superai/dayofglory/models/PicAnnotation.java
@@ -1,5 +1,12 @@
 package superai.dayofglory.models;
 
 public class PicAnnotation {
+    private String tag;
+    public String getTag() {
+        return tag;
+    }
 
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
